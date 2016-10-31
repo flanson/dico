3818b5b92198d810691e7467a02ff8b5862d96cb
druxstr <druxstr@users.noreply.github.com>
2016-11-01 00:35:18 +0100

    Very -> Most Simple

--- acf-wysiwyg-filter.php
+++ acf-wysiwyg-filter.php
@@ -6,14 +6,12 @@ add_filter( 'acf/fields/wysiwyg/toolbars' , 'my_toolbars'  );
 function my_toolbars( $toolbars ) {
 
 	// Add a new toolbar called "Very Simple"
-	// - this toolbar has only 1 row of buttons
 	$toolbars['Very Simple' ] = array();
 	$toolbars['Very Simple' ][1] = array('link', 'unlink', 'bold', 'italic' , 'underline', 'bullist', 'numlist' );
 
 	// Add a new toolbar called "Most Simple"
-	// - this toolbar has only 1 row of buttons
-	$toolbars['Very Simple' ] = array();
-	$toolbars['Very Simple' ][1] = array('link', 'unlink', 'italic');
+	$toolbars['Most Simple' ] = array();
+	$toolbars['Most Simple' ][1] = array('link', 'unlink', 'italic');
 
 	// remove the 'Basic' toolbar completely
 	//unset( $toolbars['Basic' ] );
